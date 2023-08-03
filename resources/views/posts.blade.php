893941751765224a46b61e9f362fbd4d3fcf5b9d
Easton Meth <easton.meth@gmail.com>
2023-08-03 17:21:18 -0400

    Create dynamic post routes

--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -8,21 +8,21 @@
 </head>
 <body>
     <article>
-        <h1>My First Post</h1>
+        <h1><a href="/posts/my-first-post">My First Post</a></h1>
         <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Doloremque ea hic labore eum. Molestiae possimus porro cumque quidem quisquam veniam temporibus totam dolor error quis fuga perferendis, nihil saepe? Dolore.
         Suscipit aspernatur quia dolores maiores ipsum repellat eius consectetur debitis, facilis sunt vel, placeat reprehenderit provident dignissimos, qui quae minima doloremque. Dignissimos repellat possimus laudantium natus incidunt commodi iste aspernatur.
         Temporibus quos praesentium corrupti officiis unde, doloremque similique molestias optio earum nostrum repellat aspernatur non animi alias esse fugiat culpa at ad reprehenderit sapiente odit odio? Corrupti distinctio voluptas doloremque.</p>
     </article>
 
     <article>
-        <h1>My Second Post</h1>
+        <h1><a href="/posts/my-second-post">My Second Post</a></h1>
         <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Doloremque ea hic labore eum. Molestiae possimus porro cumque quidem quisquam veniam temporibus totam dolor error quis fuga perferendis, nihil saepe? Dolore.
         Suscipit aspernatur quia dolores maiores ipsum repellat eius consectetur debitis, facilis sunt vel, placeat reprehenderit provident dignissimos, qui quae minima doloremque. Dignissimos repellat possimus laudantium natus incidunt commodi iste aspernatur.
         Temporibus quos praesentium corrupti officiis unde, doloremque similique molestias optio earum nostrum repellat aspernatur non animi alias esse fugiat culpa at ad reprehenderit sapiente odit odio? Corrupti distinctio voluptas doloremque.</p>
     </article>
     
     <article>
-        <h1>My Third Post</h1>
+        <h1><a href="/posts/my-third-post">My Third Post</a></h1>
         <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Doloremque ea hic labore eum. Molestiae possimus porro cumque quidem quisquam veniam temporibus totam dolor error quis fuga perferendis, nihil saepe? Dolore.
         Suscipit aspernatur quia dolores maiores ipsum repellat eius consectetur debitis, facilis sunt vel, placeat reprehenderit provident dignissimos, qui quae minima doloremque. Dignissimos repellat possimus laudantium natus incidunt commodi iste aspernatur.
         Temporibus quos praesentium corrupti officiis unde, doloremque similique molestias optio earum nostrum repellat aspernatur non animi alias esse fugiat culpa at ad reprehenderit sapiente odit odio? Corrupti distinctio voluptas doloremque.</p>
