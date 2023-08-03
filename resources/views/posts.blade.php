c68c520b473a9189376268a286a27ddcd0d0c2f3
Easton Meth <easton.meth@gmail.com>
2023-08-03 16:50:46 -0400

    Create basic posts view

---
+++ resources/views/posts.blade.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>My Blog</title>
+    <link rel="stylesheet" href="/app.css">
+</head>
+<body>
+    <article>
+        <h1>My First Post</h1>
+        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Doloremque ea hic labore eum. Molestiae possimus porro cumque quidem quisquam veniam temporibus totam dolor error quis fuga perferendis, nihil saepe? Dolore.
+        Suscipit aspernatur quia dolores maiores ipsum repellat eius consectetur debitis, facilis sunt vel, placeat reprehenderit provident dignissimos, qui quae minima doloremque. Dignissimos repellat possimus laudantium natus incidunt commodi iste aspernatur.
+        Temporibus quos praesentium corrupti officiis unde, doloremque similique molestias optio earum nostrum repellat aspernatur non animi alias esse fugiat culpa at ad reprehenderit sapiente odit odio? Corrupti distinctio voluptas doloremque.</p>
+    </article>
+
+    <article>
+        <h1>My Second Post</h1>
+        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Doloremque ea hic labore eum. Molestiae possimus porro cumque quidem quisquam veniam temporibus totam dolor error quis fuga perferendis, nihil saepe? Dolore.
+        Suscipit aspernatur quia dolores maiores ipsum repellat eius consectetur debitis, facilis sunt vel, placeat reprehenderit provident dignissimos, qui quae minima doloremque. Dignissimos repellat possimus laudantium natus incidunt commodi iste aspernatur.
+        Temporibus quos praesentium corrupti officiis unde, doloremque similique molestias optio earum nostrum repellat aspernatur non animi alias esse fugiat culpa at ad reprehenderit sapiente odit odio? Corrupti distinctio voluptas doloremque.</p>
+    </article>
+    
+    <article>
+        <h1>My Third Post</h1>
+        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Doloremque ea hic labore eum. Molestiae possimus porro cumque quidem quisquam veniam temporibus totam dolor error quis fuga perferendis, nihil saepe? Dolore.
+        Suscipit aspernatur quia dolores maiores ipsum repellat eius consectetur debitis, facilis sunt vel, placeat reprehenderit provident dignissimos, qui quae minima doloremque. Dignissimos repellat possimus laudantium natus incidunt commodi iste aspernatur.
+        Temporibus quos praesentium corrupti officiis unde, doloremque similique molestias optio earum nostrum repellat aspernatur non animi alias esse fugiat culpa at ad reprehenderit sapiente odit odio? Corrupti distinctio voluptas doloremque.</p>
+    </article>
+</body>
+</html>
