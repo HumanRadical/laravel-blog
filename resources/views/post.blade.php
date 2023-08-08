8f5d91d90bd36c521be84cf1c2bf325c6e2d1ccb
Easton Meth <easton.meth@gmail.com>
2023-08-08 19:09:47 -0400

    Update post blade

--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -8,7 +8,12 @@
 </head>
 <body>
     <article>
-        <?= $post; ?>
+        <h1>
+            <?= $post->title; ?>
+        </h1>
+        <p>
+            <?= $post->body; ?>
+        </p>
     </article>
 
     <a href="/">Back to posts</a>
