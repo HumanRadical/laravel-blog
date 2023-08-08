73e211d4584a741d00857298409c55bb388e9d84
Easton Meth <easton.meth@gmail.com>
2023-08-08 19:09:23 -0400

    Update find function in Post class

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -38,10 +38,6 @@ class Post
 
     public static function find($slug)
     {
-        if (!file_exists($path = resource_path("posts/{$slug}.html"))) {
-            throw new ModelNotFoundException();
-        }
-
-        return cache()->remember("posts.{$slug}", 1200, fn() => file_get_contents($path));
+        return static::all()->firstWhere('slug', $slug);
     }
 }
