c3cef1188e0fe7dd7882766a11fff518b11a1185
Easton Meth <easton.meth@gmail.com>
2023-08-29 12:58:15 -0400

    Add username to authors

--- routes/web.php
+++ routes/web.php
@@ -34,7 +34,7 @@ Route::get('/categories/{category:slug}', function (Category $category) {
     ]);
 });
 
-Route::get('/authors/{author:id}', function (User $author) {
+Route::get('/authors/{author:username}', function (User $author) {
     return view('posts', [
         'posts' => $author->posts
     ]);
