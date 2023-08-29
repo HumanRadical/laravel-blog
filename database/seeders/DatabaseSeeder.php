75ae6738dc0b16c11d43b457a143d213b8c4a696
Easton Meth <easton.meth@gmail.com>
2023-08-28 23:49:45 -0400

    Reconfigure seeders

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -16,36 +16,11 @@ class DatabaseSeeder extends Seeder
         Category::truncate();
         Post::truncate();
 
-        $user = User::factory()->create();
-
-        $personal = Category::create([
-            'name' => 'Personal',
-            'slug' => 'personal'
+        $user = User::factory()->create([
+            'name' => 'Kanye West'
         ]);
-        $hobby = Category::create([
-            'name' => 'Hobby',
-            'slug' => 'hobby'
-        ]);
-        $work = Category::create([
-            'name' => 'Work',
-            'slug' => 'work'
-        ]);
-
-        Post::create([
-            'user_id' => $user->id,
-            'category_id' => $personal->id,
-            'title' => 'My Family Post',
-            'slug' => 'my-family-post',
-            'excerpt' => 'Lorem ipsum dolor sit amet...',
-            'body' => 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Expedita velit consectetur placeat! Doloribus unde delectus esse nulla dolores fugit? Consequuntur fugit aperiam at consectetur magni eaque. Debitis earum accusamus amet! Illum et animi tenetur natus nostrum quisquam esse suscipit earum. Quam nisi excepturi veniam, consequatur, cumque cum eius error qui, repellat aut at? Veritatis totam voluptas hic, iure rem commodi? Voluptas, nulla totam, at, repudiandae inventore nesciunt a sunt quo aliquid dolor iusto repellat saepe. Non, voluptas voluptate pariatur cumque sequi nulla officia obcaecati blanditiis quaerat modi et eum quidem?'
-        ]);
-        Post::create([
-            'user_id' => $user->id,
-            'category_id' => $work->id,
-            'title' => 'My Work Post',
-            'slug' => 'my-work-post',
-            'excerpt' => 'Lorem ipsum dolor sit amet...',
-            'body' => 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Expedita velit consectetur placeat! Doloribus unde delectus esse nulla dolores fugit? Consequuntur fugit aperiam at consectetur magni eaque. Debitis earum accusamus amet! Illum et animi tenetur natus nostrum quisquam esse suscipit earum. Quam nisi excepturi veniam, consequatur, cumque cum eius error qui, repellat aut at? Veritatis totam voluptas hic, iure rem commodi? Voluptas, nulla totam, at, repudiandae inventore nesciunt a sunt quo aliquid dolor iusto repellat saepe. Non, voluptas voluptate pariatur cumque sequi nulla officia obcaecati blanditiis quaerat modi et eum quidem?'
+        Post::factory(5)->create([
+            'user_id' => $user->id
         ]);
     }
 }
