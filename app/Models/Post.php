2f9729582a76460882f51087f94096acbe45b009
Easton Meth <easton.meth@gmail.com>
2023-09-05 16:19:44 -0400

    Change category filter to use query string

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -20,6 +20,11 @@ class Post extends Model
             $query
                 ->where('title', 'like', '%' . $search . '%')
                 ->orWhere('excerpt', 'like', '%' . $search . '%'));
+
+        $query->when($filters['category'] ?? false, fn ($query, $category) => 
+            $query->whereHas('category', fn ($query) => 
+                $query->where('slug', $category))
+        );
     }
 
     public function category()
