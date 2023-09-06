0562fbccf0dc1c0523916696f48a953fa94938ac
Easton Meth <easton.meth@gmail.com>
2023-09-06 16:30:51 -0400

    Extract search bar into its own component and make author filter compatible with search

--- resources/views/posts/_header.blade.php
+++ resources/views/posts/_header.blade.php
@@ -6,20 +6,6 @@
     <div class="space-y-2 lg:space-y-0 lg:space-x-4 mt-4">
         <x-category-dropdown/>
         <x-author-dropdown/>
-
-        <!-- Search -->
-        <div class="relative flex lg:inline-flex items-center bg-gray-100 rounded-xl px-3 py-2">
-            <form method="GET" action="/">
-                @if (request("category"))
-                    <input type="hidden" name="category" value="{{ request('category') }}">
-                @endif
-
-                <input type="text" 
-                        name="search" 
-                        placeholder="Find something"
-                        class="bg-transparent placeholder-black font-semibold text-sm"
-                        value="{{ request('search') }}">
-            </form>
-        </div>
+        <x-search-bar/>        
     </div>
 </header>
