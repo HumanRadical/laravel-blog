974322d9df7fdba15d456f49d99bb2d927774215
Easton Meth <easton.meth@gmail.com>
2023-09-07 16:41:32 -0400

    Create login endpoint and functionality

--- app/Http/Controllers/SessionsController.php
+++ app/Http/Controllers/SessionsController.php
@@ -2,8 +2,32 @@
 
 namespace App\Http\Controllers;
 
+use Illuminate\Validation\ValidationException;
+
 class SessionsController extends Controller
 {
+    public function create()
+    {
+        return view('sessions.create');
+    }
+
+    public function store()
+    {
+        $attributes = request()->validate([
+            'username' => ['required', 'exists:users,username'],
+            'password' => ['required']
+        ]);
+
+        if (auth()->attempt($attributes)) {
+            session()->regenerate();
+            return redirect('/')->with('success', 'Welcome back!');
+        }
+
+        throw ValidationException::withMessages([
+            'username' => 'We were not able to log you in with the provided credentials.'
+        ]);
+    }
+
     public function destroy()
     {
         auth()->logout();
