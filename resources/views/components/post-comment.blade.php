6dcfd32ad251d73ef712ebab417648defbd2428a
Easton Meth <easton.meth@gmail.com>
2023-09-08 16:05:31 -0400

    Create comment markup

---
+++ resources/views/components/post-comment.blade.php
@@ -0,0 +1,16 @@
+<article class="flex bg-gray-100 border border-gray-200 p-6 rounded-xl space-x-4">
+    <div class="flex-shrink-0">
+        <img src="https://i.pravatar.cc/60" alt="" width="60" height="60" class="rounded-xl">
+    </div>
+
+    <div>
+        <header class="mb-4">
+            <h3 class="font-bold">Jonathan Doeman</h3>
+            <p> Posted <time>8 minutes ago</time> </p>
+        </header>
+
+        <p>
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Rerum laudantium quod sit ipsam at similique, quaerat, repellat recusandae facilis necessitatibus porro nobis suscipit voluptatem ab, voluptatibus tenetur quam vel laboriosam.
+        </p>
+    </div>
+</article>
