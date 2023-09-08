6cdcf1b9b34ae66092ac6d60aad2757a5d983a1a
Easton Meth <easton.meth@gmail.com>
2023-09-08 18:13:11 -0400

    Update text on instances of log in

--- resources/views/sessions/create.blade.php
+++ resources/views/sessions/create.blade.php
@@ -2,7 +2,7 @@
     <section class="px-6 py-8">
         <main class="max-w-lg mx-auto mt-10 bg-gray-100 border border-gray-200 p-6 rounded-xl">
             <h1 class="text-center font-bold text-xl">
-                Log In
+                Log in to Laracasts
             </h1>
 
             <form method="POST" action="/login" class="mt-10">
