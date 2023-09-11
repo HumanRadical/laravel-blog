a3878c474f8f221ec441681ab2fb1f4a982aaf9f
Easton Meth <easton.meth@gmail.com>
2023-09-11 11:32:11 -0400

    Extract controller and service

---
+++ app/Http/Controllers/NewsletterController.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Services\Newsletter;
+use Dotenv\Exception\ValidationException;
+use Illuminate\Http\Request;
+
+class NewsletterController extends Controller
+{
+    public function __invoke(Newsletter $newsletter)
+    {
+        request()->validate(['email' => ['required', 'email']]);
+
+        try {
+            $newsletter->subscribe(request('email'));
+        } catch (\Exception $e) {
+            throw ValidationException::withMessages([
+                'email' => 'Your email could not be added to our newsletter list.'
+            ]);
+        }
+
+        return redirect('/')
+        ->with('success', 'Thank you for signing up for our newsletter!');
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,40 +1,18 @@
 <?php
 
-use App\Http\Controllers\PostController;
 use App\Http\Controllers\CommentController;
+use App\Http\Controllers\NewsletterController;
+use App\Http\Controllers\PostController;
 use App\Http\Controllers\RegisterController;
 use App\Http\Controllers\SessionsController;
 use Illuminate\Support\Facades\Route;
-
-Route::post('newsletter', function () {
-    request()->validate(['email' => ['required', 'email']]);
-
-    $mailchimp = new \MailchimpMarketing\ApiClient();
-
-    $mailchimp->setConfig([
-        'apiKey' => config('services.mailchimp.key'),
-        'server' => 'us17'
-    ]);
-
-    try {
-        $response = $mailchimp->lists->addListMember('77d2f5f7f1', [
-           'email_address' => request('email'),
-           'status' => 'subscribed' 
-        ]);
-    } catch (\Exception $e) {
-        throw \Illuminate\Validation\ValidationException::withMessages([
-            'email' => 'Your email could not be added to our newsletter list.'
-        ]);
-    }
-    
-    return redirect('/')
-        ->with('success', 'Thank you for signing up for our newsletter!');
-});
 
 Route::get('/', [PostController::class, 'index'])->name('home');
 
 Route::get('/posts/{post:slug}', [PostController::class, 'show']);
 Route::post('/posts/{post:slug}/comments', [CommentController::class, 'store']);
+
+Route::post('/newsletter', NewsletterController::class);
 
 Route::get('register', [RegisterController::class, 'create'])->middleware('guest');
 Route::post('register', [RegisterController::class, 'store'])->middleware('guest');
